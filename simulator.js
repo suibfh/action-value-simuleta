e9fa642408bdfe3f254a98adf6579bd5b936ea49
suibfh <wcoin.game@gmail.com>
2025-04-07 10:08:24 +0900

    Delete simulator.js

--- simulator.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('run-sim').addEventListener('click', function () {
-  const results = document.getElementById('results');
-  results.innerHTML = '<p>ここにシミュレーション結果が表示されます（仮）</p>';
-});
